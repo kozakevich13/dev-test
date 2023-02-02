b1cfa98a4447a74d284e231095a84b9d9b1e9463
kozakevich13 <hanibalsmitti@gmail.com>
2023-02-02 11:41:38 +0200

    add keys for list of players

--- src/components/PlayersList/PlayersList.js
+++ src/components/PlayersList/PlayersList.js
@@ -4,17 +4,17 @@ import Player from '../Player/Player';
 function PlayersList() {
 
     const players = [
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
-        {name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '1', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '2', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '3', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '4', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '5', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '6', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
+        {id: '7', name: "Michael Jordan, CHI", team: "Chicago Bulls"},
     ]
 
   return (
-    <div className="PlayersList">
+    <div className="main-players-block">
         <h4 className='header-list'>Pick one of the player</h4>
         <div className='playerslist'>
             <Player listOfPlayers = {players}/>
