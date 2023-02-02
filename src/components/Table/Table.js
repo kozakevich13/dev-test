985cb8069ae03dce5a61b629dcefdc78aa38282e
kozakevich13 <hanibalsmitti@gmail.com>
2023-02-02 11:03:05 +0200

    add table

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -2,8 +2,30 @@ import './Table.css';
 
 function Table() {
   return (
-    <div className="Table">
-            TABLE
+    <div className="table">
+      <div className='main-header'>
+        <p className='header-table'>NBA</p>
+        <h2 className='header-table'>players board</h2>
+      </div>
+      
+      <table className="main-table">
+        <tr className='name-column'>
+          <th id='name'>NAME</th>
+          <th id='column'>TEAM</th>
+          <th id='column'>GP</th>
+          <th id='column'>MIN</th>
+          <th id='column'>PTS</th>
+          <th id='column'>FG</th>
+        </tr>
+        <tr>
+          <td>Andrew Jordan, CHI</td>
+          <td>Chicago Bulls</td>
+          <td>4</td>
+          <td>45</td>
+          <td>63</td>
+          <td>23%</td>
+        </tr>
+      </table>
     </div>
   );
 }
